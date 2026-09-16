2443d699bbb420520a94f0b681f7a2692e0b066d
Michisuni <aliceolkoch@gmail.com>
2026-09-15 21:08:18 -0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -106,7 +106,7 @@ function init() {
   document.querySelector('.image.selector').insertAdjacentElement('beforeend', document.createElement('select'));
 
   /** Initialize image quantity selector for results. */
-  for (let i = 0; i <= 10; i++) {
+  for (let i = 0; i <= 20; i++) {
     const select = document.createElement('option');
     select.value = i;
     select.text = i;
@@ -471,7 +471,7 @@ function progressBar(indicator, percentage) {
  * 
  * @param {number} [imageNum=3] Number of images to display. Defaults to 3.
  */
-function result(imageNum = 3) {
+function result(imageNum = 20) {
   document.querySelectorAll('.finished.button').forEach(el => el.style.display = 'block');
   document.querySelector('.image.selector').style.display = 'block';
   document.querySelector('.time.taken').style.display = 'block';
